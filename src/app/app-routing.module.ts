ff89487a2fed5ac5a6e5ef5798156b4530891abb
Patricio Antonio Diaz Letelier <Patricio.Diaz@aiep.cl>
2022-10-05 10:41:31 -0300

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Components
+import { AgregarEditarMascotaComponent } from './components/agregar-editar-mascota/agregar-editar-mascota.component';
+import { ListadoMascotaComponent } from './components/listado-mascota/listado-mascota.component';
+import { VerMascotaComponent } from './components/ver-mascota/ver-mascota.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'listMascotas', pathMatch: 'full'},
+  {path: 'listMascotas', component: ListadoMascotaComponent},
+  {path: 'agregarMascota', component: AgregarEditarMascotaComponent},
+  {path: 'verMascota/:id', component: VerMascotaComponent},
+  {path: 'editarMascota/:id', component: AgregarEditarMascotaComponent},
+  {path: '**', redirectTo: 'listMascotas', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
